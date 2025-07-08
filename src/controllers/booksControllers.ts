55276dd53fcdde8024d9548b63e2387ecb31143e
k-hroma <rociomendonca@gmail.com>
2025-07-08 14:31:06 -0300

    desarrollo de los controladores para las peticiones PATCH y DELETE

--- src/controllers/booksControllers.ts
+++ src/controllers/booksControllers.ts
@@ -115,8 +115,57 @@ const addBook = async (req: Request<{}, {}, AddBookBody>, res: Response<QueryRes
   }
 };
 
-const updateBook = async (req: Request, res: Response<QueryResponse>, next: NextFunction): Promise<void> => { 
+const updateBook = async (req: Request<{ id: string }, {}, UpdateBookBody>, res: Response<QueryResponse>, next: NextFunction): Promise<void> => { 
+  // desestructuracion de parametros
+  const { id } = req.params
+  // verificar que exista ID
+  if (!id) {
+    const errMsg = "Book ID is required."
+    res.status(400).json({
+      success: false,
+      message: errMsg
+    })
+    console.error(errMsg)
+    return
+  };
+
+  // verificar que el ID tenga el formato correcto
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({
+    success: false,
+    message: "Invalid ID format"
+  })
+  return
+  };
+
+  // valido que los datos tengan el formato del updateBookSchema con zod
+  const parseResult = UpdateBookSchema.safeParse(req.body);
+   if (!parseResult.success) {
+    res.status(400).json({
+      success: false,
+      message: "Invalid input data",
+      error: parseResult.error.errors
+    });
+    return;
+  }
+  //si el formato es correcto almaceno en una variable los datos recibidos en el body
+  const updateDataBook = parseResult.data;
   try {
+    const updatedBook = await Book.findByIdAndUpdate(id, updateDataBook, { new: true });
+    if (!updatedBook) {
+      res.status(404).json({
+        success: false,
+        message: `Book with ID ${id} not found.`}
+      );
+    return;
+    };
+
+    res.status(200).json({
+      success: true,
+      message: "Book successfully updated",
+      data: updatedBook
+    });
+    return
     
   } catch (error: unknown) {
     next(error)
@@ -125,8 +174,43 @@ const updateBook = async (req: Request, res: Response<QueryResponse>, next: Next
 };
 
 const deleteBook = async (req: Request, res: Response<QueryResponse>, next: NextFunction): Promise<void> => { 
+  // desestructuración de parámetros
+  const { id } = req.params
+  
+  // verificar que exista ID
+  if (!id) {
+    const errMsg = "Book ID is required."
+    res.status(400).json({
+      success: false,
+      message: errMsg
+    })
+    console.error(errMsg)
+    return
+  };
+
+  // verificar que el ID tenga el formato correcto
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(400).json({
+    success: false,
+    message: "Invalid ID format"
+  })
+  return
+  };
+
   try {
-    
+    const deletedBook = await Book.findByIdAndDelete(id)
+    if (!deletedBook) {
+        res.status(404).json({
+        success: false,
+        message: `Book with ID ${id} not found.`
+        });
+      return;
+      }
+    res.status(200).json({
+      success: true,
+      message: "🗑️ Book successfully deleted.",
+      data: deletedBook
+    });
   } catch (error: unknown) {
     next(error)
     return
