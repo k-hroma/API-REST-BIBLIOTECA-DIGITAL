7b71d39061442da4d2f4a8ee1ee83acbb5468cc7
k-hroma <rociomendonca@gmail.com>
2025-07-08 12:59:06 -0300

    definí interfaces para los libros, las respuestas de las peticiones y para los errores

--- src/controllers/booksControllers.ts
+++ src/controllers/booksControllers.ts
@@ -1,14 +1,15 @@
 import { Request, Response, NextFunction } from "express"
+import { QueryResponse } from "../types/queryResponse"
 
-const getBooks = async (req: Request, res: Response, next: NextFunction): Promise<void> => { }
+const getBooks = async (req: Request, res: Response<QueryResponse>, next: NextFunction): Promise<void> => { }
 
-const getBooksById = async (req: Request, res: Response, next: NextFunction): Promise<void> => { }
+const getBooksById = async (req: Request, res: Response<QueryResponse>, next: NextFunction): Promise<void> => { }
 
-const addBook = async (req: Request, res: Response, next: NextFunction): Promise<void> => { }
+const addBook = async (req: Request, res: Response<QueryResponse>, next: NextFunction): Promise<void> => { }
 
-const updateBook = async (req: Request, res: Response, next: NextFunction): Promise<void> => { }
+const updateBook = async (req: Request, res: Response<QueryResponse>, next: NextFunction): Promise<void> => { }
 
-const deleteBook = async (req: Request, res: Response, next: NextFunction): Promise<void> => { }
+const deleteBook = async (req: Request, res: Response<QueryResponse>, next: NextFunction): Promise<void> => { }
 
 
 export { getBooks, getBooksById, addBook, updateBook, deleteBook}
---
+++ src/types/queryResponse.ts
@@ -0,0 +1,9 @@
+import { IBook } from "./bookInterface"
+interface QueryResponse { 
+  success: boolean,
+  message: string,
+  data?: IBook | IBook[] | null,
+  error?: any
+}
+
+export {QueryResponse }
